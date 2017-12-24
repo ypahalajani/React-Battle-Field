dfb84b44de15e26f10b2caab49b7cbe0f6839b22
Yash Pahalajani <yash.pahalajani@innovaccer.com>
2017-12-24 19:46:31 +0530

    All the three routes completed.

---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component {
+	render () {
+		return (
+			<div className='home-container'>
+			BATTLE!
+			</div>
+		);
+	}
+}
+
+module.exports = Battle; 
