deea65c323adc4eeab217edcebd5c31933afdcea
Yash Pahalajani <ypahalajani@gmail.com>
2017-12-25 13:54:29 +0530

    Added helper methods for Battle interface

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,85 @@
 var axios = require('axios');
 
+// TODO: setup ids for permissions error when hitting github apis
+var id = 'YOUR_CLIENT_ID';
+var sec = 'YOUR_SECRET_ID';
+var params = [
+	'?client_id=',
+	id,
+	'&client_secret=',
+	sec
+	].join('');
+
+function getProfile(username) {
+	return axios.get(
+			[
+				'https://api.github.com/users',
+				username,
+				params
+			].join('')
+		)
+		.then(function (user) {
+			return user.data;
+		});
+}
+
+function getRepos(username) {
+	return axios.get(
+			[
+				'https://api.github.com/users',
+				username,
+				'/repos',
+				params,
+				'&per_page=100'
+			]
+		);
+}
+
+function getStarCount(repos) {
+	return repos.data.reduce(function (count, repo) {
+		return count + repo.stargazers_count;
+	}, count);
+}
+
+function calculateScore(profile, repos) {
+	var followers = profile.followers;
+	var totalStars = getStarCount(repos);
+
+	return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+	console.warn(error);
+	return null;
+}
+
+function getUserData(player) {
+	return axios.all([
+		getProfile(player),
+		getRepos(player)
+	]).then(function (data) {
+		var profile = data[0];
+		var repos = data[1];
+		return {
+			profile: profile,
+			score: calculateScore(profile, repos)
+		}
+	})
+}
+
+function sortPlayers(players) {
+	return players.sort(function (a, b) {
+		return b.score - a.score;
+	})
+}
+
 module.exports = {
+	battle: function (players) {
+		return axios.all(players.map(getUserData)
+			.then(sortPlayers)
+			.catch(handleError);
+		)
+	},
 	fetchPopularRepos: function (language) {
 		var encodedURI = window.encodeURI(
 			[
